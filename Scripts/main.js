7c4d80998beef8836838b7f4a442b8f4a3293749
Tomás Mayr <tmayr@tmayr.com>
2020-12-24 12:13:18 -0300

    fix offset math with more than 2 cursors

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,6 +2,10 @@ nova.commands.register("tm.paste", async (editor, text, start) => {
   const clipboard = await nova.clipboard.readText();
   const parts = clipboard.split("\n");
 
+  // since we're modifying the ranges as we're pasting
+  // we need to add to our rangeStart whatever we pasted
+  // before to keep the cursor position correct
+  let rangeOffset = 0;
   editor.edit((edit) => {
     editor.selectedRanges.forEach((range, i) => {
       // we assume each range will have a part to paste
@@ -11,15 +15,8 @@ nova.commands.register("tm.paste", async (editor, text, start) => {
       // don't paste anything
       if (!part) return;
 
-      // since we're modifying the ranges as we're pasting
-      // we need to add to our rangeStart whatever we pasted
-      // before to keep the cursor position correct
-      let rangeStart = range.start;
-      if (i > 0) {
-        rangeStart = range.start + parts[i - 1].length;
-      }
-
-      edit.insert(rangeStart, part);
+      edit.insert(range.start + rangeOffset, part);
+      rangeOffset = rangeOffset + part.length;
     });
   });
 });
