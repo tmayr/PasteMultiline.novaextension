523f0a42e7c30014da59bd602552b7f55fb1c0e8
Tomás Mayr <tmayr@tmayr.com>
2020-12-29 16:23:17 -0300

    use double quotes as multiline delimitators

--- Scripts/main.js
+++ Scripts/main.js
@@ -9,11 +9,22 @@ nova.commands.register("tm.paste", async (editor, text, start) => {
   editor.edit((edit) => {
     editor.selectedRanges.forEach((range, i) => {
       // we assume each range will have a part to paste
-      const part = parts[i];
+      let part = parts.shift();
 
-      // if we selected more lines than parts available
-      // don't paste anything
+      // if there are no more parts available, exit
       if (!part) return;
+
+      // if we have double quotes surrounding thie string, we assume it's a multiline string
+      if (part.startsWith('"')) {
+        // since we split before by line, we need to find out when the closing quote is
+        do {
+          part += parts.shift();
+        } while (!part.endsWith('"'));
+
+        // remove the first and last char corresponding to quotes
+        part = part.substring(1);
+        part = part.substring(0, part.length - 1);
+      }
 
       edit.insert(range.start + rangeOffset, part);
       rangeOffset = rangeOffset + part.length;
